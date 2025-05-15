0e253dd8c688169377047687166cdf5b7c9eedfb
seonhyelin <seonseon933@gmail.com>
2025-05-15 18:59:09 +0900

    setting commit

---
+++ src/main/java/com/docker/gestbookinfra/GestbookInfraApplication.java
@@ -0,0 +1,13 @@
+package com.docker.gestbookinfra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestbookInfraApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GestbookInfraApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/docker/gestbookinfra/GestbookInfraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docker.gestbookinfra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestbookInfraApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
